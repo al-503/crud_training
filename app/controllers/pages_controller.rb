be3efe4c0614652c04c930eec7809582c2a8ecb3
axel ridray <axel.ridray@sfr.fr>
2021-12-08 11:56:12 +0100

    création de l index dans la home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
-
+#ici l'index n'étant pas dans une page séparé je l'ai placer dans la home mais il reste un index
   def home
+    @restaurants = Restaurant.all
   end
 end
