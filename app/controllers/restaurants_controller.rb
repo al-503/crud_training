c952ffc2266f461784df71fd3ad732e853065029
axel ridray <axel.ridray@sfr.fr>
2021-12-08 12:08:54 +0100

    restaurant show

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,9 @@
 class RestaurantsController < ApplicationController
+  
+  def index
+#ici l'indes est obligatoire meme si sont contenue est dans la home page
+  end
+
   def show
     @restaurant = Restaurant.find(params[:id])
   end
