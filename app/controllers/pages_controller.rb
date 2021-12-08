9bbe33da45f5c293ca3ac6252963c09d22c78ae6
axel ridray <axel.ridray@sfr.fr>
2021-12-08 14:20:39 +0100

    changement de place bouton create

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
 skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    redirect_to restaurants_path
+    redirect_to restaurants_path #ici commande pour que la home affiche directement l index des restaurants
   end
 end
