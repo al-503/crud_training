a2c1b253c564724b6701c03c50626531b7ab287d
axel ridray <axel.ridray@sfr.fr>
2021-12-10 18:24:07 +0100

    validate speciality

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,7 @@
 class Restaurant < ApplicationRecord
   validates :name, presence: true
+  validates :name, length: { minimum: 2 }
+
+  validates :speciality, presence: true
+  validates :speciality, length: { minimum: 2 }
 end
